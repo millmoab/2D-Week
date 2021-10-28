af6614cdeb3c5eae2ba1acd0a2739e7e6521ddd5
millmoab <millmoab@iu.edu>
2021-10-28 15:32:03 -0400

    added stuff

--- 2D-Week-/Assets/Scripts/Player/PlayerController.cs
+++ 2D-Week-/Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigidBody2D;
+
+    public float runSpeed= 5;
+    public float jumpSpeed= 200f;
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Jump"))
+        {
+            int levelMask= LayerMask.GetMask("Level");
+            if(Physics2D.BoxCast(transform.position, new Vector2(1,.1f),0f, Vector2.down,.01f,levelMask) )
+            {
+                Jump();
+            }
+        }
     }
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector2 direction= new Vector2(horizontalInput * runSpeed * Time.deltaTime, 0);
+        rigidBody2D.AddForce(new Vector2(horizontalInput, 0));
+    }
+
+    void Jump()
+    {
+        rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpSpeed);
+    }
+
 }
